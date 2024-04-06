75794889ab39fa3f42dd16ee5761fd1d617051e7
Andrey <berenrayyt@gmail.com>
2024-04-07 00:23:37 +0200

    page-home debug test

--- page-home/js/filter.js
+++ page-home/js/filter.js
@@ -1,5 +1,12 @@
 window.addEventListener('DOMContentLoaded', () => {
-  location.reload();
+    const reloadFlag = localStorage.getItem('reloadFlag');
+
+    if (!reloadFlag) {
+        localStorage.setItem('reloadFlag', 'true');
+        location.reload();
+    } else {
+        localStorage.removeItem('reloadFlag'); // Опционально, чтобы удалить флаг после первой загрузки
+    }
 
   (function() {
     const elem = document.querySelector(".my-projects");
