5bb3b43c61c795b7f506dbe76ec5861666dc34d0
Andrey <berenrayyt@gmail.com>
2024-04-22 01:25:34 +0200

    page-nursery version 1.05

--- page-nursery/js/script.js
+++ page-nursery/js/script.js
@@ -89,4 +89,113 @@ window.addEventListener('DOMContentLoaded', () => {
         image.src = "content/contacts/youtube hover.png";
     });
 
+    // Animation
+
+    const windowHeight = window.innerHeight,
+        menu = document.querySelector('.menu-text'),
+        firstText = document.querySelectorAll('.first-text_animation'),
+        firstSection = document.querySelector('.nursery-content_first'),
+        secondSection = document.querySelector('.nursery-content_second'),
+        thirdSection = document.querySelector('.nursery-content_third'),
+        buyBtn = document.querySelector('.buy'),
+        textMail = document.querySelector('.text-contacts'),
+        mail = document.querySelector('.uniForm'),
+        textContacts = document.querySelectorAll('.text-contacts_animation'),
+        contactsContent = document.querySelector('.contacts-information_sections');
+
+    let index = 0;
+
+    menu.classList.remove('menu_animation_before');
+    menu.classList.add('menu_animation');
+    firstSection.classList.remove('img-animation_before');
+    firstSection.classList.add('img-animation');
+
+    const intervalFirstText = setInterval(()=>{
+        firstText[index].classList.remove('img-animation_before');
+        firstText[index].classList.add('img-animation');
+        index++;
+        if(index >= firstText.length){
+            clearInterval(intervalFirstText)
+        }
+    }, 50);
+
+
+    window.addEventListener('scroll', ()=> {
+        const secondSectionElement = document.querySelector('.second-section-animation'),
+            secondSectionElementPosition =secondSectionElement.getBoundingClientRect().top;
+
+        if(secondSectionElementPosition < windowHeight){
+            secondSection.classList.remove('img-animation-second_before');
+            secondSection.classList.add('img-animation-second');
+        }
+    });
+
+    window.addEventListener('scroll', ()=>{
+        const thirdSectionElement = document.querySelector('.third-section-animation'),
+            thirdSectionElementPosition = thirdSectionElement.getBoundingClientRect().top;
+
+        if(thirdSectionElementPosition < windowHeight){
+            thirdSection.classList.remove('img-animation_before');
+            thirdSection.classList.add('img-animation');
+        }
+    });
+
+    window.addEventListener('scroll', ()=> {
+        const buyBtnElement = document.querySelector('.backg-contacts'),
+            buyBtnElementPosition = buyBtnElement.getBoundingClientRect().top;
+
+        if(buyBtnElementPosition< windowHeight){
+            buyBtn.classList.remove('buy-btn-animate_before');
+            buyBtn.classList.add('buy-btn-animate');
+        }
+    });
+    window.addEventListener('scroll', ()=> {
+        const textMailElement = document.querySelector('.uniForm'),
+            textMailElementPosition = textMailElement.getBoundingClientRect().top;
+
+        if(textMailElementPosition < windowHeight){
+            textMail.classList.remove('animate_text_mail_before');
+            textMail.classList.add('animate_text_mail');
+        }
+    });
+
+    window.addEventListener('scroll', ()=> {
+        const mailElement = document.querySelector('.mail-animation'),
+            mailElementPosition = mailElement.getBoundingClientRect().top;
+
+        if(mailElementPosition < windowHeight) {
+            mail.classList.remove('menu_animation_before');
+            mail.classList.add('menu_animation');
+        }
+    });
+
+    window.addEventListener('scroll', ()=> {
+        const textContactsElement = document.querySelector('.phone'),
+            textContactsElementPosition = textContactsElement.getBoundingClientRect().top;
+
+        let index = 0;
+
+
+        if(textContactsElementPosition < windowHeight) {
+            const interval = setInterval(()=> {
+                textContacts[index].classList.remove('menu_animation_before');
+                textContacts[index].classList.add('menu_animation');
+                index++;
+                if(index >= textContacts.length) {
+                    clearInterval(interval)
+                }
+            }, 30)
+        }
+    });
+
+    window.addEventListener('scroll', ()=> {
+        const contactsContentElement = document.querySelector('.address'),
+            contactsContentElementPosition = contactsContentElement.getBoundingClientRect().top;
+
+        if(contactsContentElementPosition < windowHeight){
+            contactsContent.classList.remove('img-animation_before');
+            contactsContent.classList.add('img-animation');
+        }
+    });
+
 })
