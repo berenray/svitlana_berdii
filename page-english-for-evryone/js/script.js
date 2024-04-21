2932a2e6900d560b011e822de05ee2d34b4b5f81
Andrey <berenrayyt@gmail.com>
2024-04-21 19:43:37 +0200

    page-english-for-everyone version 1.03

--- page-english-for-evryone/js/script.js
+++ page-english-for-evryone/js/script.js
@@ -89,4 +89,103 @@ window.addEventListener('DOMContentLoaded', () => {
         image.src = "content/contacts/youtube hover.png";
     });
 
+    // Animation    
+
+    const windowHeight = window.innerHeight,
+        menu = document.querySelector('.menu-text'),
+        firstText = document.querySelectorAll('.first-text-animtaion'),
+        firstSection = document.querySelector('.english-content_first'),
+        secondSection = document.querySelector('.english-content_second'),
+        thirdSection = document.querySelector('.english-content_third'),
+        textMail = document.querySelector('.text-contacts'),
+        mail = document.querySelector('.uniForm'),
+        textContacts = document.querySelectorAll('.text-contacts_animation'),
+        contactsContent = document.querySelector('.contacts-information_sections');
+
+    let index = 0;
+
+    menu.classList.remove('menu_animation_before');
+    menu.classList.add('menu_animation');
+    firstSection.classList.remove('img-animation_before');
+    firstSection.classList.add('img-animation');
+
+
+    const intervalFirstText = setInterval(()=> {
+        firstText[index].classList.remove('menu_animation_before');
+        firstText[index].classList.add('menu_animation');
+        index++;
+        if(index >= firstText.length) {
+            clearInterval(intervalFirstText);
+        }
+    }, 30);
+
+    window.addEventListener('scroll', ()=> {
+        const secondSectionElement = document.querySelector('.second-section-animation'),
+            secondSectionElementPosition = secondSectionElement.getBoundingClientRect().top;
+
+        if(secondSectionElementPosition < windowHeight) {
+            secondSection.classList.remove('img-animation-second_before');
+            secondSection.classList.add('img-animation-second');
+        }
+    });
+
+    window.addEventListener('scroll', ()=> {
+        const thirdSectionElement = document.querySelector('.third-section-animation'),
+            thirdSectionElementPosition = thirdSectionElement.getBoundingClientRect().top;
+
+        if(thirdSectionElementPosition < windowHeight) {
+            thirdSection.classList.remove('img-animation_before');
+            thirdSection.classList.add('img-animation');
+        }
+    });
+
+    window.addEventListener('scroll', ()=> {
+        const textMailElement = document.querySelector('.uniForm'),
+            textMailElementPosition = textMailElement.getBoundingClientRect().top;
+
+        if(textMailElementPosition < windowHeight){
+            textMail.classList.remove('animate_text_mail_before');
+            textMail.classList.add('animate_text_mail');
+        }
+    });
+
+    window.addEventListener('scroll', ()=> {
+        const mailElement = document.querySelector('.mail-animation'),
+            mailElementPosition = mailElement.getBoundingClientRect().top;
+
+        if(mailElementPosition < windowHeight) {
+            mail.classList.remove('menu_animation_before');
+            mail.classList.add('menu_animation');
+        }
+    });
+
+    window.addEventListener('scroll', ()=> {
+        const textContactsElement = document.querySelector('.phone'),
+            textContactsElementPosition = textContactsElement.getBoundingClientRect().top;
+
+        let index = 0;
+
+
+        if(textContactsElementPosition < windowHeight) {
+            const interval = setInterval(()=> {
+                textContacts[index].classList.remove('menu_animation_before');
+                textContacts[index].classList.add('menu_animation');
+                index++;
+                if(index >= textContacts.length) {
+                    clearInterval(interval)
+                }
+            }, 30)
+        }
+    });
+
+    window.addEventListener('scroll', ()=> {
+        const contactsContentElement = document.querySelector('.address'),
+            contactsContentElementPosition = contactsContentElement.getBoundingClientRect().top;
+
+        if(contactsContentElementPosition < windowHeight){
+            contactsContent.classList.remove('img-animation_before');
+            contactsContent.classList.add('img-animation');
+        }
+    })
+
 })
