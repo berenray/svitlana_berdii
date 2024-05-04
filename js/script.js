42a2c9bad07c3df0cad377ad3e42f5f4895e450a
Andrey <berenrayyt@gmail.com>
2024-05-04 23:44:22 +0200

    version 1.0.3

--- js/script.js
+++ js/script.js
@@ -60,29 +60,58 @@ tabsParent.addEventListener('click', (event) => {
 
     //Mail
 
-    const mailPath = 'mail.php'
+    const mailPath = 'mail.php',
+    modal = document.querySelector('.modal'),
+    modalDialog = document.querySelector('.modal__dialog');
+
+    function opanThanksModal() {
+        modal.style.display = 'flex';
+        document.body.style.overflow = 'hidden';
+        modalDialog.classList.add('fade-modal');
+        setTimeout (()=> {
+            modal.style.display = 'none';
+            document.body.style.overflow = '';
+        }, 15000)
+    }
+
+    function closeModalThanks () {
+        modal.style.display = 'none';
+        document.body.style.overflow = '';
+    }
+
 
     document.querySelectorAll('.uniForm').forEach( (e) => {
 
-	e.addEventListener('submit', function(e) {
+    e.addEventListener('submit', function(e) {
 
-		let th      = this,
-		    params  = new FormData(this),
-		    request = new XMLHttpRequest()
+        let th = this,
+            params = new FormData(this),
+            request = new XMLHttpRequest();
 
-		request.open('POST', mailPath, true)
-		request.send(params)
+        request.open('POST', mailPath, true)
+        request.send(params)
 
-		request.onreadystatechange = function() {
-			if (this.readyState == 4 && this.status == 200) {
-				setTimeout(function() { th.reset() }, 1000)
-				alert('Thank you!')
-			}
-		}
+        request.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                opanThanksModal();
+                th.reset();
+            }
+        }
+        e.preventDefault();
 
-		e.preventDefault()
+        });
 
-	});
+        modal.addEventListener('click', (event)=> {
+            if(event.target == modal || event.target.getAttribute('data-close') == '') {
+                closeModalThanks();
+            }
+        })
+
+        document.addEventListener('keydown', (event)=> {
+            if(event.code === 'Escape' && modal.style.display == 'flex') {
+                closeModalThanks();
+            }
+        })
 
     });
 
